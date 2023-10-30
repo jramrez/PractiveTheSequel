148d5195549e04b93f01de151ea987e4310899b2
jramrez <jramrez@ucvts.org>
2023-10-30 09:22:49 -0400

    ujpate3, the betterer

--- app/src/main/java/com/example/practivethesequel/MainActivity.kt
+++ app/src/main/java/com/example/practivethesequel/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.practivethesequel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.practivethesequel.ui.theme.PractiveTheSequelTheme
@@ -31,37 +33,48 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize(),
                     color = MaterialTheme.colorScheme.onBackground
                 ) {
-                    WakeUp("Android")
+                    RedText("Android")
                 }
             }
         }
     }
 }
 
-var testingVar: Int = 0
+// W = 412 H = 892
 
 @Composable
-fun WakeUp(name: String, modifier: Modifier = Modifier) {
-        Surface(color = Color.Transparent) {
+fun StarTest(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.white_star_hi)
+    Image (
+        painter = image,
+        contentDescription = null,
+        modifier = modifier
+            .offset(x = 230.dp, y = 20.dp)
+    )
+}
+
+@Composable
+fun RedText(name: String, modifier: Modifier = Modifier) {
+        Surface(color = Color.White) {
             Text(
-                text = "Wakey wakey $name! Wakey Wakey! Wakey Wakey! Wakey Wakey!",
+                text = "$name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name",
                 modifier = modifier
-                    .size(width = 412.dp, height = 892.dp)
-                    .offset(x = 0.dp, y = 21.dp)
+                    .size(width = 206.dp, height = 892.dp)
+                    .offset(x = 0.dp, y = 0.dp)
                     .background(Color.Red)
             )
         }
 }
 
 @Composable
-fun Greet(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Green) {
+fun BlueText(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Transparent) {
         Text(
-            text = "Welcome Welcome Welcome $name",
+            text = "$name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name $name",
             modifier = modifier
-                .size(width = 412.dp, height = 20.dp)
-                .offset(x = 50.dp, y = 0.dp)
-                .background(Color.Transparent)
+                .size(width = 412.dp, height = 206.dp)
+                .offset(x = 206.dp, y = 0.dp)
+                .background(Color.Blue)
         )
     }
 }
@@ -70,7 +83,8 @@ fun Greet(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PractiveTheSequelTheme {
-        WakeUp("Android")
-        Greet(name = "Android")
+        RedText("Android")
+        BlueText(name = "Android")
+        StarTest()
     }
 }
